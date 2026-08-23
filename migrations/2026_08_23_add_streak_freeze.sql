c77d8fa24b5037336319431230e9f97e811b21c1
Samuel-Didier <lawsonsamuel9198@gmail.com>
2026-08-23 15:21:40 -0400

    Définir deux gels disponibles par défaut

--- migrations/2026_08_23_add_streak_freeze.sql
+++ migrations/2026_08_23_add_streak_freeze.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users
     ADD COLUMN streak_count INT NOT NULL DEFAULT 0,
     ADD COLUMN last_activity_date DATE NULL,
-    ADD COLUMN streak_freezes_available INT NOT NULL DEFAULT 0;
+    ADD COLUMN streak_freezes_available INT NOT NULL DEFAULT 2;
